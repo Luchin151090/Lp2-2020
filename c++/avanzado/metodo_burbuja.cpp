bfd7e110114704d497522a7ccbef45ada30769b8
Luchin <64605259+Luchin151090@users.noreply.github.com>
2020-06-09 10:59:54 -0500

    ORDENAMIENTO BURBUJA

---
+++ c++/avanzado/metodo_burbuja.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+void burbuja(int a[],int n)
+{ int aux=0;
+  for(int i=0;i<n;i++)
+  {for(int j=0;j<n-1;j++)
+    { if(a[j]>a[j+1])
+      { aux=a[j];
+        a[j]=a[j+1];
+        a[j+1]=aux;
+      }
+    }
+  }
+}
+int main()
+{ int a[]={5,3,4,1,2};
+  burbuja(a,5);
+  for(int i=o;i<5;i++)
+  {cout<<a[i]<<" ";
+  }
+  return 0;
+}
