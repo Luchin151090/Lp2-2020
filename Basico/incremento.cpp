deb98eeae88e1d52f2e8b5f6707d1b4906f073e8
Luchin <64605259+Luchin151090@users.noreply.github.com>
2020-10-29 14:26:27 -0500

    incremento

---
+++ Basico/incremento.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{	
+	int m = 10 ;
+	int n;
+  
+	//operador prefijo-incremento
+	cout<<"operador prefijo-incremento"<<endl;
+	n = ++m; //incrementas "m" en 1 y asignas asi mismo
+			     //luego la asignas a "n" .
+	cout<<"m = "<<m<<" n = "<<n<<endl;// m = 11, n = 11
+	cout<<endl;
+  
+	//operador postfijo-incremento
+	cout<<"operador postfijo-incremento"<<endl;
+	n = m++; //asignas a "n" primero el valor de "m" (n = 11)
+			     //luego "m" se incrementa en 1.
+	cout<<"m = "<<m<<" n = "<<n<<endl;// m = 12, n = 11
+	
+	
+	int d = 5;
+	int e;
+	e = ++d * --d; // ++d vale 6, luego --d vale 5
+	cout<<e<<endl; // e = 25 
+	// ¿ por que no vale e = 30 ?
+	
+  
+  return 0;
+  }
